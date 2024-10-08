25ba76fa738335d724a4c093a55d1e8df5b08a1a
itsara <khajer@gmail.com>
2024-10-08 17:13:02 +0700

    parse value Id

---
+++ src/routes/id/[id]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+
+	// Access the id parameter from the URL
+	const id = $page.params.id;
+</script>
+
+<h1>Detail Page for ID: {id}</h1>
+
+<p>This is the detail page for the item with ID: {id}.</p>
